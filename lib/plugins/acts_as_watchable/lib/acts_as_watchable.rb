54ab0ccc42d0f7d0ab5000511387f8daedf9bbe5
Jens Ulferts <j.ulferts@finn.de>
2014-06-09 13:28:21 +0200

    wip - possible_watcher_users

--- lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
+++ lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
@@ -62,6 +62,9 @@ module Redmine
                 :conditions => ["#{Watcher.table_name}.user_id = ?", user_id] }
             }
             attr_protected :watcher_ids, :watcher_user_ids if accessible_attributes.nil?
+
+            class_attribute :acts_as_watchable_options
+            self.acts_as_watchable_options = options
           end
           send :include, Redmine::Acts::Watchable::InstanceMethods
           alias_method_chain :watcher_user_ids=, :uniq_ids
@@ -86,13 +89,12 @@ module Redmine
         end
 
         def possible_watcher_users
-          User.not_builtin.tap do |users|
-            if respond_to?(:visible?)
-              users.select! {|user| possible_watcher?(user)}
-            else
-              warn watching_permitted_to_all_users_message
-            end
-          end
+          permission = self.class.acts_as_watchable_options[:permission]
+
+          User.allowed(permission, self.project, admin_pass: false)
+              .not_builtin
+              .order_by_name
+              .select_only_name_attributes
         end
 
         # Returns an array of users that are proposed as watchers
