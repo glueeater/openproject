d25903358550c49254672170c83fb5a54ee0e6d6
Alex Coles <alex@alexbcoles.com>
2014-07-04 13:43:11 +0200

    Remove unnecessary Composition in WorkPackageModel
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/api/v3/work_packages/work_package_model.rb
+++ lib/api/v3/work_packages/work_package_model.rb
@@ -34,7 +34,6 @@ module API
   module V3
     module WorkPackages
       class WorkPackageModel < Reform::Form
-        include Composition
         include Coercion
         include ActionView::Helpers::UrlHelper
         include OpenProject::TextFormatting
@@ -45,106 +44,101 @@ module API
         # N.B. required by ActionView::Helpers::UrlHelper
         def controller; nil; end
 
-        model :work_package
+        property :subject,          type: String
+        property :start_date,       type: Date
+        property :due_date,         type: Date
+        property :created_at,       type: DateTime
+        property :updated_at,       type: DateTime
+        property :author,           type: String
+        property :project_id,       type: Integer
+        property :responsible_id,   type: Integer
+        property :assigned_to_id,   type: Integer
+        property :fixed_version_id, type: Integer
 
-        property :subject, on: :work_package, type: String
-        property :start_date, on: :work_package, type: Date
-        property :due_date, on: :work_package, type: Date
-        property :created_at, on: :work_package, type: DateTime
-        property :updated_at, on: :work_package, type: DateTime
-        property :author, on: :work_package, type: String
-        property :project_id, on: :work_package, type: Integer
-        property :responsible_id, on: :work_package, type: Integer
-        property :assigned_to_id, on: :work_package, type: Integer
-        property :fixed_version_id, on: :work_package, type: Integer
-
-        def work_package
-          model[:work_package]
-        end
 
         def description
-          format_text(work_package, :description)
+          format_text(model, :description)
         end
 
         def raw_description
-          work_package.description
+          model.description
         end
 
         def raw_description=(value)
-          work_package.description = value
+          model.description = value
         end
 
         def type
-          work_package.type.try(:name)
+          model.type.try(:name)
         end
 
         def type=(value)
           type = Type.find(:first, conditions: ['name ilike ?', value])
-          work_package.type = type
+          model.type = type
         end
 
         def status
-          work_package.status.try(:name)
+          model.status.try(:name)
         end
 
         def status=(value)
           status = Status.find(:first, conditions: ['name ilike ?', value])
-          work_package.status = status
+          model.status = status
         end
 
         def priority
-          work_package.priority.try(:name)
+          model.priority.try(:name)
         end
 
         def priority=(value)
           priority = IssuePriority.find(:first, conditions: ['name ilike ?', value])
-          work_package.priority = priority
+          model.priority = priority
         end
 
         def estimated_time
-          { units: 'hours', value: work_package.estimated_hours }
+          { units: 'hours', value: model.estimated_hours }
         end
 
         def estimated_time=(value)
           hours = ActiveSupport::JSON.decode(value)['value']
-          work_package.estimated_hours = hours
+          model.estimated_hours = hours
         end
 
         def version_id=(value)
-          work_package.fixed_version_id = value
+          model.fixed_version_id = value
         end
 
         def percentage_done
-          work_package.done_ratio
+          model.done_ratio
         end
 
         def percentage_done=(value)
-          work_package.done_ratio = value
+          model.done_ratio = value
         end
 
         def author
-          ::API::V3::Users::UserModel.new(work_package.author)  unless work_package.author.nil?
+          ::API::V3::Users::UserModel.new(model.author)  unless model.author.nil?
         end
 
         def responsible
-          ::API::V3::Users::UserModel.new(work_package.responsible) unless work_package.responsible.nil?
+          ::API::V3::Users::UserModel.new(model.responsible) unless model.responsible.nil?
         end
 
         def assignee
-          ::API::V3::Users::UserModel.new(work_package.assigned_to) unless work_package.assigned_to.nil?
+          ::API::V3::Users::UserModel.new(model.assigned_to) unless model.assigned_to.nil?
         end
 
         def activities
-          work_package.journals.map{ |journal| ::API::V3::Activities::ActivityModel.new(journal) }
+          model.journals.map{ |journal| ::API::V3::Activities::ActivityModel.new(journal) }
         end
 
         def attachments
-          work_package.attachments
+          model.attachments
             .map{ |attachment| ::API::V3::Attachments::AttachmentModel.new(attachment) }
         end
 
         def watchers
-          work_package.watcher_users
+          model.watcher_users
             .order(User::USER_FORMATS_STRUCTURE[Setting.user_format])
             .map{ |u| ::API::V3::Users::UserModel.new(u) }
         end
--- lib/api/v3/work_packages/work_package_representer.rb
+++ lib/api/v3/work_packages/work_package_representer.rb
@@ -50,82 +50,82 @@ module API
         property :_type, exec_context: :decorator
 
         link :self do
-          { href: "#{root_url}api/v3/work_packages/#{represented.work_package.id}", title: "#{represented.subject}" }
+          { href: "#{root_url}api/v3/work_packages/#{represented.model.id}", title: "#{represented.subject}" }
         end
 
         link :author do
           {
-              href: "#{root_url}/api/v3/users/#{represented.work_package.author.id}",
-              title: "#{represented.work_package.author.name} - #{represented.work_package.author.login}"
-          } unless represented.work_package.author.nil?
+              href: "#{root_url}/api/v3/users/#{represented.model.author.id}",
+              title: "#{represented.model.author.name} - #{represented.model.author.login}"
+          } unless represented.model.author.nil?
         end
 
         link :responsible do
           {
-              href: "#{root_url}/api/v3/users/#{represented.work_package.responsible.id}",
-              title: "#{represented.work_package.responsible.name} - #{represented.work_package.responsible.login}"
-          } unless represented.work_package.responsible.nil?
+              href: "#{root_url}/api/v3/users/#{represented.model.responsible.id}",
+              title: "#{represented.model.responsible.name} - #{represented.model.responsible.login}"
+          } unless represented.model.responsible.nil?
         end
 
         link :assignee do
           {
-              href: "#{root_url}/api/v3/users/#{represented.work_package.assigned_to.id}",
-              title: "#{represented.work_package.assigned_to.name} - #{represented.work_package.assigned_to.login}"
-          } unless represented.work_package.assigned_to.nil?
+              href: "#{root_url}/api/v3/users/#{represented.model.assigned_to.id}",
+              title: "#{represented.model.assigned_to.name} - #{represented.model.assigned_to.login}"
+          } unless represented.model.assigned_to.nil?
         end
 
         link :availableWatchers do
             {
-                href: "#{root_url}api/v3/work_packages/#{represented.work_package.id}/available_watchers",
+                href: "#{root_url}api/v3/work_packages/#{represented.model.id}/available_watchers",
                  title: "Available Watchers"
             }
         end
 
         link :watch do
           {
-              href: "#{root_url}/api/v3/work_packages/#{represented.work_package.id}/watchers",
+              href: "#{root_url}/api/v3/work_packages/#{represented.model.id}/watchers",
               method: :post,
               data: { user_id: @current_user.id },
               title: 'Watch work package'
           } if !@current_user.anonymous? &&
-             current_user_allowed_to(:view_work_packages, represented.work_package) &&
-            !represented.work_package.watcher_users.include?(@current_user)
+             current_user_allowed_to(:view_work_packages, represented.model) &&
+            !represented.model.watcher_users.include?(@current_user)
         end
 
         link :unwatch do
           {
-              href: "#{root_url}/api/v3/work_packages/#{represented.work_package.id}/watchers/#{@current_user.id}",
+              href: "#{root_url}/api/v3/work_packages/#{represented.model.id}/watchers/#{@current_user.id}",
               method: :delete,
               title: 'Unwatch work package'
-          } if current_user_allowed_to(:view_work_packages, represented.work_package) && represented.work_package.watcher_users.include?(@current_user)
+          } if current_user_allowed_to(:view_work_packages, represented.model) && represented.model.watcher_users.include?(@current_user)
         end
 
         link :addWatcher do
           {
-              href: "#{root_url}/api/v3/work_packages/#{represented.work_package.id}/watchers{?user_id}",
+              href: "#{root_url}/api/v3/work_packages/#{represented.model.id}/watchers{?user_id}",
               method: :post,
               title: 'Add watcher',
               templated: true
-          } if current_user_allowed_to(:add_work_package_watchers, represented.work_package)
+          } if current_user_allowed_to(:add_work_package_watchers, represented.model)
         end
 
-        property :id, getter: -> (*) { work_package.id }, render_nil: true
+        property :id, getter: -> (*) { model.id }, render_nil: true
         property :subject, render_nil: true
         property :type, render_nil: true
         property :description, render_nil: true
         property :raw_description, render_nil: true
         property :status, render_nil: true
         property :priority, render_nil: true
-        property :start_date, getter: -> (*) { work_package.start_date }, render_nil: true
-        property :due_date, getter: -> (*) { work_package.due_date }, render_nil: true
+        property :start_date, getter: -> (*) { model.start_date }, render_nil: true
+        property :due_date, getter: -> (*) { model.due_date }, render_nil: true
         property :estimated_time, render_nil: true
         property :percentage_done, render_nil: true
-        property :version_id, getter: -> (*) { work_package.fixed_version.try(:id) }, render_nil: true
-        property :version_name,  getter: -> (*) { work_package.fixed_version.try(:name) }, render_nil: true
-        property :project_id, getter: -> (*) { work_package.project.id }
-        property :project_name, getter: -> (*) { work_package.project.try(:name) }
-        property :created_at, getter: -> (*) { work_package.created_at.utc.iso8601}, render_nil: true
-        property :updated_at, getter: -> (*) { work_package.updated_at.utc.iso8601}, render_nil: true
+        property :version_id, getter: -> (*) { model.fixed_version.try(:id) }, render_nil: true
+        property :version_name,  getter: -> (*) { model.fixed_version.try(:name) }, render_nil: true
+        property :project_id, getter: -> (*) { model.project.id }
+        property :project_name, getter: -> (*) { model.project.try(:name) }
+        property :created_at, getter: -> (*) { model.created_at.utc.iso8601}, render_nil: true
+        property :updated_at, getter: -> (*) { model.updated_at.utc.iso8601}, render_nil: true
 
         collection :custom_properties, exec_context: :decorator, render_nil: true
 
@@ -143,11 +143,11 @@ module API
         end
 
         def watchers
-          represented.watchers.map{ |watcher| ::API::V3::Users::UserRepresenter.new(watcher, work_package: represented.work_package, current_user: @current_user) }
+          represented.watchers.map{ |watcher| ::API::V3::Users::UserRepresenter.new(watcher, work_package: represented.model, current_user: @current_user) }
         end
 
         def custom_properties
-            values = represented.work_package.custom_field_values
+            values = represented.model.custom_field_values
             values.map { |v| { name: v.custom_field.name, format: v.custom_field.field_format, value: v.value }}
         end
 
